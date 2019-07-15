f9edadc3f83cf445e6d7c37b77bd786e02310477
Rachael <rdougherty031@gmail.com>
2019-07-15 00:32:07 -0400

    create page design

--- client/src/pages/create.js
+++ client/src/pages/create.js
@@ -2,15 +2,19 @@ import React from "react";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import TripData from "../components/TripData";
-import TopInterests from "../components/TopInterests";
+import PlanitBtn from "../components/PlanitBtn";
 
 export default function CreateTrip() {
     return (
         <div>
             <Header />
             <Navbar />
-            <TripData />
-            <TopInterests />
+            <div id="createTripWrapper">
+                <div id="createInnerWrapper">
+                    <TripData />
+                    <PlanitBtn />
+                </div>
+            </div>
         </div>
     );
 }
